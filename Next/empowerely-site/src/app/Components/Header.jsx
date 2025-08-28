054e44b00e2da9be193db7d3d01f6c4a13817dff
Orazio WebDev <oraziocastellank@gmail.com>
2025-08-28 09:44:44 +0200

    Update Header.jsx
    props title

--- Next/empowerely-site/src/app/Components/Header.jsx
+++ Next/empowerely-site/src/app/Components/Header.jsx
@@ -1,9 +1,9 @@
 import Nav from './Nav'
 
-function Header (props) {
+function Header ({title}) {
     return (
         <header className="header">
-            <h1>Empowerely</h1>
+            <h1>{title}</h1>
             <img src="../../public/mini Empowerely" alt="Empowerely Logo" className="logo" />
             <Nav />
         </header>
